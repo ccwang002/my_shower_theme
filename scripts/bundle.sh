7475f26248bdbb9c11a54abcb66668169ee70f81
Liang-Bo Wang <liang-bo.wang@wustl.edu>
2022-05-11 15:39:03 +0800

    Add command to publish to gh-pages

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
-
+rm -rf bundled
 mkdir -p bundled/lib
+
+touch bundled/.nojekyll
+
+rsync -a node_modules/@shower/core/dist/ bundled/lib/shower
+rsync -a node_modules/@highlightjs/cdn-assets/ bundled/lib/highlightjs
+rsync -a node_modules/katex/dist/ bundled/lib/katex
+
 rsync -a \
     styles fonts fonts pictures \
     .gitattributes .gitignore \
@@ -8,7 +15,4 @@ rsync -a \
     LICENSE.md README.md \
     bundled/
 
-rsync -a node_modules/@shower/core/dist/ bundled/lib/shower
-rsync -a node_modules/@highlightjs/cdn-assets/ bundled/lib/highlightjs
-rsync -a node_modules/katex/dist/ bundled/lib/katex
 sed -f scripts/fix_npm_pkg_paths.sed index.html > bundled/index.html
