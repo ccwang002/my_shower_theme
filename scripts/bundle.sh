300bc61e69b8dc465280171c09e206661802c779
Liang-Bo Wang <liang-bo.wang@wustl.edu>
2022-05-11 18:41:22 +0800

    Remove unncessary files in the bundle output

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -1,18 +1,49 @@
 #!/usr/bin/env bash
+set -euxo pipefail
+
 rm -rf bundled
 mkdir -p bundled/lib
 
+# Don't use Jekyll to build the github pages
 touch bundled/.nojekyll
 
-rsync -a node_modules/@shower/core/dist/ bundled/lib/shower
-rsync -a node_modules/@highlightjs/cdn-assets/ bundled/lib/highlightjs
-rsync -a node_modules/katex/dist/ bundled/lib/katex
 
+# Bundle @shower/core
+rsync -a node_modules/@shower/core/dist/ bundled/lib/shower
+
+
+# Bundle highlight.js
+rsync -a node_modules/@highlightjs/cdn-assets/ bundled/lib/highlightjs
+# remove unneccessary files
+rm -rf bundled/lib/highlightjs/{es/,highlight.js,package.json}
+# keep only one style
+rm -rf bundled/lib/highlightjs/styles/base16
+find bundled/lib/highlightjs/styles \
+    -type f \
+    ! -name 'github.min.css' \
+    -exec rm -- {} +
+
+
+# Bundle KaTeX
+rsync -a node_modules/katex/dist/ bundled/lib/katex
+# remove unneccessary files
+rm -rf \
+    bundled/lib/katex/fonts/*.ttf \
+    bundled/lib/katex/katex.{mjs,js,css} \
+# keep only minimized js and CSS files
+find bundled/lib/katex/contrib \
+    -type f \
+    -depth 1 \
+    ! -name '*.min.js' ! -name '*.min.css' \
+    -exec rm -- {} +
+
+
+# Copy theme related files
 rsync -a \
     styles fonts fonts pictures \
     .gitattributes .gitignore \
     .editorconfig .stylelintrc \
     LICENSE.md README.md \
     bundled/
-
+# fix the library file paths in the bundle
 sed -f scripts/fix_npm_pkg_paths.sed index.html > bundled/index.html
